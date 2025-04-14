fa884921eff03f286bc9c83cb7e852418bc5866f
Hamza <hamzasamadh@gamil.com>
2025-04-14 23:08:29 +0500

    Added Python code for variables

---
+++ variable.py
@@ -0,0 +1,17 @@
+a=30
+b=20
+c=30
+
+print(a,b,c)
+
+
+print(id(a),id(b),id(c))
+# iss ka answer 30 20 30 aur id aegi in teeno ki
+# variables usse kehten hen jissme ham value store krten hen aur zaroorat perne per variable k naam ko call krten hen tw usski value print hoti he jesa
+# ki opper example me dikhaya gaya he 
+
+
+
+
+
+
